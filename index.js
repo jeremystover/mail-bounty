bc8d57d5efd52c3ac1bb3582c6439e2e64eab3bd
Jeremy Stover <jsstover@gmail.com>
2019-04-03 15:00:01 -0700

    added express

--- index.js
+++ index.js
@@ -21,3 +21,40 @@ api.connect().then(() => {
 }).then(() => {
   console.log('done and disconnected.');
 }).catch(console.error);
+
+
+/*
+TODO:
+
+1) account balance
+
+2) pay (from,to)
+
+3) Generate a new account??
+
+*/
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+
