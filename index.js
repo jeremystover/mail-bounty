727b01ed2d0284f77227db963f8fe6c165248a8e
Jeremy Stover <jsstover@gmail.com>
2019-07-23 21:02:07 -0700

    make it better

--- index.js
+++ index.js
@@ -2,17 +2,28 @@
 var XRPLWS = require('./xrplws');
 var xrplws = new XRPLWS(process.env.XRPL_SOCKET_SERVER, process.env.APP_XRPL_ACCOUNT);
 xrplws.on('PaymentReceived', function(error, pmt) {
-	db.get(pmt.sender, function (err, acct) {
+	db.get(pmt.sender, function (err, email) {
 		var new_balance = 0;
-		if (err || acct===null) {
-			//account doesn't exist
-			acct = {'balance':pmt.amount,'confirmViaEmail':true, 'email':''};
+		if (err || email===null) {
+			//TODO: account link doesn't exist - ideally we'd return the payment... or at least send myself an email so I know it is 'lost'
+			
 		} else {
-			acct = JSON.parse(acct);
-			acct.balance = acct.balance + pmt.amount;
+			db.get(email, function(err, acct) {
+				if (err || acct===null) {
+					//create account object
+					acct = {'balance':0,'confirmViaEmail':true, 'xrplAddress':pmt.sender};
+				} else {
+					acct = JSON.parse(acct);
+					if (acct.xrplAddress!=pmt.sender) {
+						//TODO: raise an error!
+					}
+				}
+				
+				acct.balance = acct.balance + pmt.amount;
+				db.set(email, JSON.stringify(acct));
+				console.log("Incoming payment added to account balance.");
+			});
 		}
-		db.set(pmt.sender, JSON.stringify(acct));
-		console.log("Incoming payment added to account balance.");
 	});
 });
 xrplws.on("wsDisconnect", function(data) {
@@ -26,15 +37,17 @@ var XRPL = require('./xrpl');
 var xrpl = new XRPL(process.env.XRPL_SERVER, process.env.APP_XRPL_ACCOUNT, process.env.APP_XRPL_SECRET);
 
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_EMAILER_KEY);
 
 /*
 db objects:
 
 Emails:
-emailaddr:xrplAccountNumber
+xrplAccountNumber: emailaddr
 
 Accounts:
-xrplAccountNumber:{balance,confirmViaEmailBoolean,email}
+emailaddr: {balance,confirmViaEmailBoolean,xrplAccount Number}
 
 Bounties:
 messageId:{amount,expires,paidDate,recipientHash,senderHash}
@@ -73,7 +86,7 @@ passport.use(new GoogleStrategy({
     //});
 	console.log('login success');
 	console.log(profile);
-	return cb(null, {"email":profile._json.email});
+	return cb(null, profile._json.email);
   }
 ));
 app.use(passport.initialize());
@@ -87,17 +100,7 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-
-// set the view engine to ejs
-
-
-// make express look in the public directory for assets (css/js/img)
-
 function loggedIn(req, res, next) {
-	console.log(req);
     if (req.user) {
         next();
     } else {
@@ -105,71 +108,133 @@ function loggedIn(req, res, next) {
     }
 }
 
-
-app.get('/place', loggedIn, function(req, res) {
-	//const messageId = req.body.messageId;
-	//const validHours = req.body.validHours;
-	//const recipientEmail = req.body.recipient;
-	//const senderEmail = req.body.sender;
-	res.send(req.user);
+app.post('/place', loggedIn, function(req, res) {
+	const messageId = req.body.messageId;
+	const validHours = req.body.validHours;
+	const recipientEmail = req.body.recipient;
+	const amt = req.body.amount;
+	const senderEmail = req.user; //not a variable only something we get back from gmail to make sure it is legit.
 	//messageId:{amount,expires,paidDate,recipientHash,senderHash}
-	//vulnerable in that anyone could create a bounty and claim it for themselves...
-	
+	//validate input vars??
+	//TODO: Future version; put amounts for bounties in escrow or on hold to avoid double spending?  For now just confirm sufficient balance exists.
+	db.get(senderEmail, function(err, acct) {
+		if (err || acct===null) {
+			res.send("Account not found.  Bounty not created");
+			return;
+		}
+		acct = JSON.parse(acct);
+		if (acct.balance < amt) {
+			res.send("Insufficient funds.  Bounty not created");
+			return;
+		}
+		db.set(messageId, JSON.stringify({'sender':senderEmail, 'recipient':recipientEmail, 'expires':new Date().addHours(validHours),'amount':amount, 'paidDate':''}));
+		res.send("Bounty created");
+	});	
 });
 
-app.post('/claim', loggedIn, function(req, res) {
+app.post('/pay', loggedIn, function(req, res) {
+	const messageId = req.body.messageId;
+	const validHours = req.body.validHours;
+	const recipientEmail = req.body.recipient;
+	const senderEmail = req.user;
 	
-	
+	db.get(messageId, function(err, bounty) {
+		if (err || bounty===null) {
+			res.send("Failed: Bounty doesn't exist for this message.");
+			return;
+		}
+		bounty = JSON.parse(bounty);
+		if (new Date() > bounty.expires) {
+			res.send("Failed: Bounty expired.");
+			return;
+		}
+		if (bounty.recipient != recipientEmail) {
+			res.send("Failed: Recipient not included on original bounty.");
+			return;
+		}
+		if (bounty.sender != senderEmail) {
+			res.send("Failed: Bounty not issued by you.");
+			return;
+		}
+		//all ok to pay bounty
+		db.get(senderEmail, function(err, sAccount) {
+			if (err || sAccount===null) {
+				res.send("Failed.  No account exists for sender's email (0)."); //can't create one for the sender as it will have 0 balance...
+				return;
+			}
+			sAccount = JSON.parse(sAccount);
+			if (sAccount.balance < bounty.amount) {
+				res.send("Failed.  Insufficient Funds (0)."); //can't create one for the sender as it will have 0 balance...
+				return;
+			}
+			sAccount.balance = sAccount.balance - bounty.amount;
+				
+			db.get(recipientEmail, function(err, rAccount) {
+				if (err || rAccount===null) {
+					//no account exists yet so create one... they can link later...
+					rAccount = {'balance':0,'confirmViaEmail':true, 'xrplAddress':pmt.sender};
+				} else {
+					rAccount = JSON.parse(rAccount);
+				}
+				rAccount.balance = rAccount.balance + bounty.amount;
+				db.set(senderEmail, JSON.stringify(sAccount));
+				db.set(recipientEmail, JSON.stringify(rAccount));
+					
+				const msg = {
+				  to: recipientEmail,
+				  from: "xrpemailbounty@ripple.com",
+				  subject: "You've got XRP",
+				  text: senderEmail + ' has put a bounty on an email you responded to.  Visit ' + _WebUrl + '/cashout to claim your ' + bounty.amount + ' XRP or create an account so you can send XRP bounties to get responses to your emails.',
+				  html: senderEmail + " has put a bounty on an email you responded to.  Visit <a href='" + _WebUrl + "/cashout'>" + _WebUrl + "/cashout</a> to claim your " + bounty.amount + ' XRP or create an account so you can send XRP bounties to get responses to your emails.'
+				};
+				sgMail.send(msg);
+				
+				
+				res.send("Bounty paid successfully.");
+			});
+		}
+		
+	})
 });
 
 app.post('/link', loggedIn, function(req, res) {
-	//TODO: Link email address to xrpl Account Number so we're set to receive payment.
 	const email = req.body.email;
 	const acctId = req.body.account;
 	
-	//check to see if we have an account already
-	db.get(acctId, function(error, data) {
-		if (error || data === null) { //we do not
+	//check to see if this email is linked to an account already
+	db.get(acctId, function(error, linkedEmail) {
+		if (error || linkedEmail === null) { //we do not
 			//check to see if this email is registered to another account
 			db.get(email, function(err, acct) {
-				if (err || data === acct) { // we do not... this is a new link. create it.
-					db.set(email, acctId);
-					db.set(acctId, "{'balance':0,'confirmViaEmailBoolean':'true','email':'" + email + "'}");
+				if (err || acct === null) { // we do not... this is a new link. create it.
+					db.set(email, "{'balance':0,'confirmViaEmailBoolean':'true','xrplAccount':'" + acctId + "'}");
 					res.send('Success: Ledger account linked to email address.');
 					return;
-				} else { //account linked to another id...  possibly handle in the future 
-					res.send('Error: Ledger account already linked to another email address (0).');
+				} else { //account exists for this, but not linked.  Create the link
+					db.set(acctId, email);
+					res.send('Success: Ledger account linked to email address. (1)');
 					return;
 				}
 			});
-		} else {
-			var acct = JSON.parse(data);
-			if (!acct.email || acct.email=="") {
-			
-				//check to see if email is registered
-				db.get(email, function(err, acct) {
-					if (err || data === acct) { // we do not... this is a new link. create it.
-						db.set(email, acctId);
-						acct.email = email;
-						db.set(acctId, JSON.stringify(acct));
-						res.send('Success: Ledger account linked to email address.');
+		} else { //linked account exists...
+			db.get(email, function(err, acct) {
+				if (err || acct === null) { // we do not have an account.  Create one...
+					db.set(email, "{'balance':0,'confirmViaEmailBoolean':'true','xrplAccount':'" + acctId + "'}");
+					res.send('Success: Ledger account linked to email address.');
+					return;
+				} else { //we do have a link and an account. Confirm they match.
+					acct = JSON.parse(acct);
+					if (acct.xrplAccount == acctId && linkedEmail == email) {
+						res.send('Success: Ledger account already linked to this email address (1).');
 						return;
-					} else { //we do have a link, but not an account.  create one.
-						db.set(acctId, "{'balance':0,'confirmViaEmailBoolean':'true','email':'" + email + "'}");
+					} else {
 						res.send('Error: Ledger account already linked to another email address (1).');
 						return;
 					}
-				});
-			} else { //there is an account with an email.  Need to throw an error here as it is not clear what to do.  
-				//add the link to the existing email.
-				db.set(acct.email, acctId);
-				res.send('Error: Ledger account already linked to another email address (2).');
-				return;
-			}
+				}
+			});
 		}
-		
 	});
-	
 });
 
 //cash out...  suseptible in that only need email address to do but they can only send the payment to the account associated with the addresss...
@@ -178,38 +243,27 @@ app.post('/out', loggedIn, function (req, res) {
 	const email = req.body.email;
 	const amt = req.body.amount;
 	
-	db.get(email, function(error, acctId) {
-		if (error || acctId===null) {
+	db.get(email, function(error, acct) {
+		if (error || acct===null) {
 			res.send('Account not found (1).');
 			return;
 		}
-		db.get(acctId, function(err, acct) {
-			if (err || acct===null) {
-				res.send('Account not found (2).');
+		acct = JSON.parse(acct);
+		if (acct.balance < amt) {
+			res.send("Insufficient balance.");
+			return;
+		}
+		xrpl.send(acct.xrplAccount, amt, function(result) {
+			if (!result) {
+				res.send("Payment failed.");
 				return;
 			}
-			acct = JSON.parse(acct);
-			if (acct.balance < amt) {
-				res.send("Insufficient balance.");
-				return;
-			}
-			xrpl.send(acctId, amt, function(result) {
-				if (!result) {
-					res.send("Payment failed.");
-					return;
-				}
-				acct.balance = acct.balance - amt;
-				db.set(acctId, JSON.stringify(acct));
-				res.send("Success.  Payment sent.");
-			});
+			acct.balance = acct.balance - amt;
+			db.set(email, JSON.stringify(acct));
+			res.send("Success.  Payment sent.");
 		});
-		
 	});
-})
-
-
-
-
+});
 
 app.get('/login', passport.authenticate('google', { scope: ['email'] }));
 
@@ -228,7 +282,10 @@ app.listen(port, function() {
 });
 
 
-
+Date.prototype.addHours = function(h) {
+  this.setTime(this.getTime() + (h*60*60*1000));
+  return this;
+}
 
 /*
 TODO:
@@ -244,17 +301,17 @@ X Connect to Redis to update balance amounts
 X ** XRPL transaction to receiver address
 ---
 GMAIL Integration:
-Pay out bounty (verify it exists, Connect to Redis and store/retreive bounties, mark completed, if confirm via email send email to recipient)
-Verify balance exists and return hash to include in email - queried via ajax from extension when inserting bounty
+X Pay out bounty (verify it exists, Connect to Redis and store/retreive bounties, mark completed, if confirm via email send email to recipient)
+X Verify balance exists and return hash to include in email - queried via ajax from extension when inserting bounty
 ---
 Settings:
-xrpl account #
-confirm via email on
-Figure out most secure secret storage on herkoku
+X xrpl account #
+X Figure out most secure secret storage on herkoku
 
 
 
 Future consideration:
+confirm via email on
 
 Could a chrome extension securely operate and sign transactions locally?  If so, we could operate directly on ledger.
 Guidelines for supporting an exchange and cold wallet security
