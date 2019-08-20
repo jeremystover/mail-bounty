192bbabe001d2b99cf819b31024b358edf81081f
Jeremy Stover <jsstover@gmail.com>
2019-08-19 22:00:59 -0700

    adding gmail oauth2

--- index.js
+++ index.js
@@ -316,7 +316,7 @@ app.post('/balance', function(req, res) {
 	const client = new OAuth2Client(["450274019939-uqlvbvslggaa6els7cp32lmvae7ls7d7.apps.googleusercontent.com", "450274019939-ecc4cpim20h7se1a55l7539414nkvurl.apps.googleusercontent.com"]);
 	async function verify() {
 	  const ticket = await client.verifyIdToken({
-	      idToken: token,
+	      idToken: req.body.token,
 	      audience: ["450274019939-uqlvbvslggaa6els7cp32lmvae7ls7d7.apps.googleusercontent.com", "450274019939-ecc4cpim20h7se1a55l7539414nkvurl.apps.googleusercontent.com"],  // Specify the CLIENT_ID of the app that accesses the backend
 	      // Or, if multiple clients access the backend:
 	      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
